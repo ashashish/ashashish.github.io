2ea204fcea7c060d6c1145f7053dec2b4aa0d96a
Ashish <59656709+ashashish@users.noreply.github.com>
2023-06-29 13:17:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-<script>
-(function(s,u,r,f,l,y){s[f]=s[f]||{init:function(){s[f].q=arguments}};
-l=u.createElement(r);y=u.getElementsByTagName(r)[0];l.async=1;
-l.src='https://surfly.com/surfly.js';y.parentNode.insertBefore(l,y);})
-(window,document,'script','Surfly');
-
-var settings = {
-    // Surfly session options can be set here, or at the Company/Plan levels.
-    widget_key: '9eb008187dca447685c55d80614e1d18',
-};
-
-Surfly.init(settings, function(initResult) {
-    if (initResult.success) {
-        // API calls can now be made!
-        if (!Surfly.isInsideSession) {
-            Surfly.button();
-        }
-    }
-    else{
-        console.log("Surfly was unable to initialize properly.")
-    }
-});
-</script>
 
 <script>
   
@@ -34,7 +11,7 @@ popup.style.transform = "translate(-50%, -50%)";
 popup.style.backgroundColor = "white";
 popup.style.padding = "20px";
 popup.style.border = "1px solid black";
-popup.style.zIndex = "9999"; // Set the z-index to a high value
+popup.style.zIndex = "2147483647"; // Set the z-index to a high value
 
 // Create the title element
 var title = document.createElement("h2");
